20d975bd87889821fb2ec6a65add475eaf06c790
Illarion <porubin.exceedteam@gmail.com>
2022-02-28 18:39:22 +0300

    trello

---
+++ client/src/components/app/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "../card/card"
+
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+      <Card></Card>
+    </div>
+  )
+}
+
+
+export default App;
